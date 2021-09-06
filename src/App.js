3b78a6b9ab3fa574f054da1d07b5411c5541335b
ALVARO P <alv.gomez@outlook.com>
2021-09-06 17:31:20 +0200

    Complete Helloworld and UserCard Redux Exercises

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
-import HelloWorld from "./HelloWorld";
+import React from "react";
+// import HelloWorld from "./HelloWorld";
+import UserCard from "./UserCard";
+import reducer from "./reducers/index";
+
+// Creating the Redux Store
+import { createStore } from "redux";
+
+/* Hello World Example */
+// const initialState = { tech: 'React' };
+
+/* Refactor User Card Example */
+const initialState = {
+	name: "Alex Bakery",
+	description: "Software Engineer, Speaker, and Occasional Model",
+	likes: "Cats, Wine, and Black dresses",
+	location: "localhost",
+};
+
+const store = createStore(reducer, initialState);
 
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+	return (
+		<div>
+			{/* <HelloWorld tech={store.getState().tech} /> */}
+			<UserCard store={store.getState()} />
+		</div>
+	);
 };
 
 export default App;
